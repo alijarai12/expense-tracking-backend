0fb94717734dddd351dc5c3642bd8c563e3254ad
alija <alijarai414@gmail.com>
2025-02-19 20:56:03 +0545

    remove dir from repo

--- expense-tracking-backend/backend/expenses/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ExpenseListCreate, ExpenseDetail
-
-urlpatterns = [
-    path('api/expenses/', ExpenseListCreate.as_view(), name='expense-list-create'),
-    path('api/expenses/<int:pk>/', ExpenseDetail.as_view(), name='expense-detail'),
-
-
-]
